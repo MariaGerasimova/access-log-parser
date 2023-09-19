c4a5751d2fe6b9ad81fb11a18d76e343629ea7c5
Maria Gerasimova <mgerasimova@inno.tech>
2023-09-20 00:05:28 +0300

    changing Main (adding different operation with two number (2.9)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1= new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число отличное от 0:");
+        int number2= new Scanner(System.in).nextInt();
+        //сумма
+        int sum=number1+number2;
+        System.out.println("Сумма двух чисел = " + sum);
+        //разность
+        int diff=number1-number2;
+        System.out.println("Разность двух чисел = " + diff);
+        //Произведение
+        int miltiplic=number1*number2;
+        System.out.println("Произведение двух чисел = " + miltiplic);
+        //Частное
+        double quetient=(double) number1/number2;
+        System.out.println("Частное двух чисел= " + quetient);
     }
 }
