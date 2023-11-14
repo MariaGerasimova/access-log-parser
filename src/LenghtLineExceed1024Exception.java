41959f41d080af44f2a10ee6792fbfd6acf3e2d5
Maria Gerasimova <mgerasimova@inno.tech>
2023-11-14 09:01:16 +0300

    comadd HW 9,reading file

---
+++ src/LenghtLineExceed1024Exception.java
@@ -0,0 +1,9 @@
+public class LenghtLineExceed1024Exception extends Exception{
+    public LenghtLineExceed1024Exception (){super ();}
+    public LenghtLineExceed1024Exception (String message){
+        super (message);
+    }
+    public LenghtLineExceed1024Exception (String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import com.sun.source.tree.WhileLoopTree;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,15 +13,42 @@ public class Main {
 
          //запрашиваем путь к файлу
         int fileCount=0;
+        String path; // вынесла инициализацию переменной (13.11)
         while (true) {
             System.out.println("Введите путь к файлу и нажмите <Enter>: ");
-            String path = new Scanner(System.in).nextLine();
+            path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists(); // проверка на существование файла и/или каталога
-            boolean isDirectory = file.isDirectory(); // проверка на то, что это дирекция
+            boolean isDirectory = file.isDirectory(); // проверка на то, что это директория
             if (fileExists==true && isDirectory==false) {
                 fileCount++;
                 System.out.println("Путь указан верно." + "Это файл номер " + fileCount + ".");
+                // #9 (новое от 13.11.2023)
+                //C:\Users\MGerasimova\Desktop\Работа\Обучение\Файл\access.log
+                try (FileReader fileReader = new FileReader(path)){
+                    //FileReader fileReader = new FileReader(path); // класс для чтения файла
+                    BufferedReader reader = new BufferedReader(fileReader); // сохранение содержимого файла (всего) в буфер для чтения
+                    String line;
+                    List<Integer> linelst=new ArrayList<>(); // массив для хранения размера строк
+                    while ((line = reader.readLine()) != null) { //readLine() - считает построчно(берет целую строку из буфера и возвращает ее как string)
+                        int length = line.length();
+                        linelst.add(length);
+                        if (length>1024) {
+                            throw new LenghtLineExceed1024Exception("Длина строки превышает 1024 символа");
+                        };
+                    }
+                    System.out.println("Общее количество строк в файле: " + linelst.size());
+                    System.out.println("Длина самой короткой строки в файле: " + minLenght(linelst));
+                    System.out.println("Длина самой длинной строки в файле: " + maxLenght(linelst));
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException ex){
+                    ex.printStackTrace();
+                } catch (LenghtLineExceed1024Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
             else if (fileExists==true && isDirectory==true) {
                 System.out.println("Путь указан к папке, а не к файлу"); continue;
@@ -28,5 +57,21 @@ public class Main {
                 System.out.println("Файл не существует или указанный путь не является путём к папке"); continue;
             }
         }
+     }
+    //метод, возврщающий наибольший размер из листа Integer
+    public static int maxLenght (List <Integer> lst){
+        int maxLenght=lst.get(0);
+        for (int i = 1; i < lst.size(); i++) {
+            if (lst.get(i) > maxLenght) maxLenght = lst.get(i);
+        }
+        return maxLenght;
+    }
+    //метод, возвращающий наибольший размер из листа Integer
+    public static int minLenght (List <Integer> lst){
+        int minLenght=lst.get(0);
+        for (int i = 1; i < lst.size(); i++) {
+            if (lst.get(i) < minLenght) minLenght = lst.get(i);
+        }
+        return minLenght;
     }
 }
